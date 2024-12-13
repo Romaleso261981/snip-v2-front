afefef661933a72c767147cf8ec5f83886331392
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-12-13 16:08:48 +0200

    fix: added some code

--- src/components/layout/GeneralLayout/GeneralLayout.tsx
+++ src/components/layout/GeneralLayout/GeneralLayout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 export default function GeneralLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="pb-10 text-gold">
+    <div className="relative pb-10 text-gold">
       <Header className="text-gold" />
       <main className="container mx-auto px-4">
         {children}
